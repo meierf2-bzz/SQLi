702c83ffaf8e018256411942493dad39076f1864
Florian Meier <florian@rmeier.ch>
2025-04-10 17:43:35 +0200

    Changes by Tino

--- sqli_demo/src/main/java/ch/bzz/sqli_demo/Users.java
+++ sqli_demo/src/main/java/ch/bzz/sqli_demo/Users.java
@@ -1,15 +1,10 @@
 package ch.bzz.sqli_demo;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Getter
-@Setter
 @Entity
 public class Users {
     @Id
@@ -18,15 +13,27 @@ public class Users {
     private String username;
     private String password;
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
